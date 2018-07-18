30e2d9b893d1cafcbd465db12af9013a8755ce61
teja <tkundaikar10@gmail.com>
2018-07-18 20:31:15 +0530

    vvvv

---
+++ circleTest.java
@@ -0,0 +1,16 @@
+package circle;
+
+import junit.framework.TestCase;
+
+public class circleTest extends TestCase {
+
+	public void testGetRadius() {
+		
+		circle c=new circle();
+		float radius=5;
+		c.setRadius(radius);
+		float g=5;
+		assertEquals(g,c.getRadius());
+	}
+
+}
