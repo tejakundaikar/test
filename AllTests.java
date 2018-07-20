b167b134149d2d3c24100fa734d685902e08955e
teja <tkundaikar10@gmail.com>
2018-07-20 05:47:41 +0530

    added file all test suite

---
+++ AllTests.java
@@ -0,0 +1,17 @@
+package circle;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite(AllTests.class.getName());
+		//$JUnit-BEGIN$
+		suite.addTestSuite(circleTest.class);
+		suite.addTestSuite(testcircle.class);
+		//$JUnit-END$
+		return suite;
+	}
+
+}
