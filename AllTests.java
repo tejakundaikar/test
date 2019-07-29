6232406f6e7ce73fa2cd8396a456e817519311a8
teja <tkundaikar10@gmail.com>
2019-07-29 19:10:08 +0530

    hi

--- AllTests.java
+++ AllTests.java
@@ -1,10 +1,10 @@
 package circle;
-//hh
+
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
 public class AllTests {
-
+//hhh
 	public static Test suite() {
 		TestSuite suite = new TestSuite(AllTests.class.getName());
 		//$JUnit-BEGIN$
